5031a0fd5048ffd2f617dddb1acb230a214f0a0a
Alex <ajstrand8@gmail.com>
2022-03-18 22:09:03 -0400

    trying something

--- Package.swift
+++ Package.swift
@@ -1,47 +1,51 @@
 // swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-#if swift(>=5.2) && !os(Linux)
-let pkgConfig: String? = nil
-#else
-let pkgConfig = "libgd-dev"
-#endif
+// #if swift(>=5.2) && !os(Linux)
+// let pkgConfig: String? = nil
+// #else
+// let pkgConfig = "libgd-dev"
+// #endif
 
-#if swift(>=5.2)
-let providers: [SystemPackageProvider] = [
-    .apt(["libgd-dev"])
-]
-#else
-let providers: [SystemPackageProvider] = [
-    .apt(["libgd-dev"]),
-    //  .brew(["libxml2"])
-]
-#endif
+// #if swift(>=5.2)
+// let providers: [SystemPackageProvider] = [
+//     .apt(["libgd-dev"])
+// ]
+// #else
+// let providers: [SystemPackageProvider] = [
+//     .apt(["libgd-dev"]),
+// ]
+// #endif
+
+
+// let package = Package(
+//         name: "GD",
+//         products: [
+//             .library(name:"GD", targets: ["GD"])
+//         ],
+    
+//         targets: [
+//              .systemLibrary(name: "cgd", pkgConfig: pkgConfig, providers: providers),
+//             .target(name: "GD", dependencies: ["cgd"])
+//         ]
+// )
 
 import PackageDescription
 
+
 let package = Package(
-        name: "GD",
-        products: [
+    name: "GD",
+   
+      products: [
             .library(name:"GD", targets: ["GD"])
         ],
     
         targets: [
-             .systemLibrary(name: "cgd", pkgConfig: pkgConfig, providers: providers),
+             .systemLibrary(name: "cgd", 
+             pkgConfig: "libgd-dev", 
+             providers: [
+        .brew(["gd"]),
+        .apt(["libgd-dev"])
+        ]),
             .target(name: "GD", dependencies: ["cgd"])
         ]
 )
-
-//         pkgConfig: "",
-// providers: [
-//   .apt(["libsqlite-dev"]),
-//   .brew(["sqlite3"])
-// ],
-
-// targets: [
-// .target(name: "myPackage"),
-// .systemLibrary(name: "libxml2",
-//                path: "Modules",
-//                pkgConfig: pkgConfig,
-//                providers: providers)
-// ])
-// )
